af0ed699202120693966ab8853cfcdc54d9763dc
Ilya R. Gorbatenko <irgorbatenko-d@rttv.ru>
2024-06-01 14:20:21 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿PrintResult(Check(Input()));
+
+
+string[] Input()
+{
+    Console.WriteLine("Введите 5 случайных строк через (Пробел):");
+    string[] arrayNumber = Console.ReadLine().Split(' ');
+    return arrayNumber;
+}
+
+
+string[] Check(string[] arr)
+{
+    int counter = 0;
+    foreach (string item in arr)
+    {
+        if (item.Length <= 3) counter++;
+    }
+    string[] NewArray = new string[counter];
+    int counter2 = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            NewArray[counter2] = arr[i];
+            counter2++;
+        }
+    }
+    return NewArray;
+}
+
+
+void PrintResult(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
