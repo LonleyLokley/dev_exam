7f517489cc6c09aff2dcbe96c526933aa0546cec
Ilya R. Gorbatenko <irgorbatenko-d@rttv.ru>
2024-06-01 13:54:25 +0300

    ChecknCreate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Check(string[] arr)
+{
+    int counter = 0;
+    foreach (string item in arr)
+    {
+        if (item.Length <= 3) counter++;
+    }
+    string[] NewArray = new string[counter];
+    int counter2 = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            System.Console.WriteLine(counter2);
+            NewArray[counter2] = arr[i];
+            counter2++;
+        }
+    }
+    return arr;
+}
+
+
+
+
